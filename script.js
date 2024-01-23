69e3d8f5a189f6135e4ddd9db7e95fa347138efc
GuiMaelby <gmaelby@hotmail.com>
2024-01-23 14:38:45 -0300

    gallery

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ document.addEventListener("DOMContentLoaded", function () {
         navList.classList.toggle('active');
     });
 });
+
+let slideIndex = 1;
+mostrarFoto(slideIndex);
+
+function mudarFoto(n) {
+    mostrarFoto(slideIndex += n);
+}
+
+function mostrarFoto(n) {
+    let slides = document.getElementsByClassName("slide");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
